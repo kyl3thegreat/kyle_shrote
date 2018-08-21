1a818fe16cfff3d1e4840bd535fcc51aea62943b
Kyle Shrote <kyle.shrote@gmail.com>
2018-08-21 17:13:31 -0400

    init

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+// new fullpage('#fullpage', {
+//     //options here
+//     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+// 	scrollOverflow: true
+// });
+
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+        navigation: false
+    });
+});

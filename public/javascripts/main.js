93a4f48c0b1b17ceb477c74090f8b78e3cdf4c00
Kyle Shrote <kyle.shrote@gmail.com>
2018-08-23 21:53:07 -0400

    mobile first

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,5 @@ $(document).ready(function() {
         verticalCentered: true,
         css3: true,
         sectionsColor: ['linear-gradient(to top, #6a85b6 0%, #bac8e0 100%)', '#FDFDFD', '#FDFDFD', 'linear-gradient(to top, #6a85b6 0%, #bac8e0 100%)'],
-        loopBottom: true,
     });
 });

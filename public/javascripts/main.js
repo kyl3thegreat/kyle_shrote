5ae67f10f98d0289f53095ad0ad3cbbb0af4bdf0
Kyle Shrote <kyle.shrote@gmail.com>
2018-09-04 19:24:38 -0400

    some updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,3 @@
-// new fullpage('#fullpage', {
-//     //options here
-//     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-// 	scrollOverflow: true
-// });
-
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
         navigation: false,
